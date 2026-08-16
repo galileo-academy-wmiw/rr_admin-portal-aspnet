5658d110270ae1ce3879edf787e4cd738764fe93
Robin Rahman <robin.rahman.nl@gmail.com>
2026-08-16 16:43:34 +0200

    Bind login form fiels to page model.

--- AdminPortal/Pages/Account/Login.cshtml.cs
+++ AdminPortal/Pages/Account/Login.cshtml.cs
@@ -1,8 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace AdminPortal.Pages.Account;
 
 public class LogInModel : PageModel
 {
-    
+    [BindProperty]
+    public string Username { get; set; } = string.Empty;
+    [BindProperty]
+    public string Password { get; set; } = string.Empty;
 }
