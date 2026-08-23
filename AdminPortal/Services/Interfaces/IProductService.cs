a0bd3d83df7d7486a4cc22f5f202f8771b64718d
Robin Rahman <robin.rahman.nl@gmail.com>
2026-08-23 17:07:29 +0200

    Add product service CRUD and database test page

--- AdminPortal/Services/Interfaces/IProductService.cs
+++ AdminPortal/Services/Interfaces/IProductService.cs
@@ -3,4 +3,7 @@ namespace AdminPortal.Services.Interfaces;
 public interface IProductService
 {
     List<Product> GetAllProducts();
+    bool AddProduct(string productName, string description, double productPrice, int quantityInStock);
+    bool UpdateProduct(int productId, string productName, string description, double productPrice, int quantityInStock);
+    bool DeleteProduct(int productId);
 }
--- AdminPortal/Services/ProductService.cs
+++ AdminPortal/Services/ProductService.cs
@@ -9,6 +9,27 @@ public class ProductService: IProductService
         _productRepository = productRepository;
     }
 
+        public bool AddProduct(string productName, string description, double productPrice, int quantityInStock)
+    {
+        int result = _productRepository.InsertProduct(productName, description, productPrice, quantityInStock);
+        return result == 1;
+    }
+
+    public bool UpdateProduct(int productId, string productName, string description, double productPrice, int quantityInStock)
+    {
+        int result = _productRepository.UpdateProduct(productId, productName, description, productPrice, quantityInStock);
+        return result > 0;
+    }
+
+    public bool DeleteProduct(int productId)
+    {
+        if (_productRepository.IsProductInUse(productId))
+            return false;
+
+        int result = _productRepository.DeleteProduct(productId);
+        return result > 0;
+    }
+
     public List<Product> GetAllProducts()
     {
         return _productRepository.GetAllProducts();
