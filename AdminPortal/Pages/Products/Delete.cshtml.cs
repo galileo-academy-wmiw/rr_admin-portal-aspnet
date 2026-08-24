45d4dc944b855d92e804fdda025ece273380aa26
Robin Rahman <robin.rahman.nl@gmail.com>
2026-08-24 02:39:45 +0200

    Add product delete page

---
+++ AdminPortal/Pages/Products/Delete.cshtml.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminPortal.Pages.Products;
+
+public class DeleteModel : PageModel
+{
+    private readonly IProductService _productService;
+
+    public Product Product { get; private set; } = null!;
+
+    public DeleteModel(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public IActionResult OnGet(int id)
+    {
+        Product? product = _productService.GetProductById(id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        Product = product;
+        return Page();
+    }
+
+    public IActionResult OnPost(int id)
+    {
+        bool productDeleted = _productService.DeleteProduct(id);
+
+        if (productDeleted)
+        {
+            return RedirectToPage("/Products/Index");
+        }
+
+        Product? product = _productService.GetProductById(id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        Product = product;
+        ModelState.AddModelError("", "The product cannot be deleted because it is used in an order.");
+        return Page();
+    }
+}
